a9136304660d66903acdde66d85726a2450a05d4
Christian Lasarczyk <christian@lasarczyk.de>
2020-04-12 14:32:53 +0200

    removed old  packages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// JS Goes here - ES6 supported
-
-import "./css/mystyles.scss";
-//import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
-
-
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
